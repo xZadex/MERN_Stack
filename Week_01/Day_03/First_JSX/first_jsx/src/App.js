9ac17451bc31d4cd9c2357f4c5924a29d2ca7599
Nicholas <codebyzade@gmail.com>
2022-10-25 00:39:05 -0400

    first react with jsx assignment completed

---
+++ Week_01/Day_03/First_JSX/first_jsx/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-center">Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ul className="list-group">
+        <li className="list-group-item">Learn React</li>
+        <li className="list-group-item">Climb Mt. Everest</li>
+        <li className="list-group-item">Run a marathon</li>
+        <li className="list-group-item">Feed the dogs</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
