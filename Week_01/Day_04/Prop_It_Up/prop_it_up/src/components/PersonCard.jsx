ea30853082063e82ad2c72a6c2504ea50c3f9b1c
Nicholas <codebyzade@gmail.com>
2022-10-26 10:06:12 -0400

    prop it up assignment completed

--- Week_01/Day_04/Prop_It_Up/prop_it_up/src/components/PersonCard.jsx
+++ Week_01/Day_04/Prop_It_Up/prop_it_up/src/components/PersonCard.jsx
@@ -1,12 +1,26 @@
 import React, {Component} from 'react';
 
+
 class PersonCard extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            age: this.props.age
+        }
+    }
+
+    addYear = () => {
+        this.setState({age: this.state.age+1});
+    }
+
+
     render(){
         return(
             <div>
                 <h1>{this.props.lastName},{this.props.firstName}</h1>
-                <p>Age: {this.props.age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color:  {this.props.hairColor}</p>
+                <button onClick={this.addYear}>Birthday button for {this.props.firstName} {this.props.lastName}</button>
             </div>
         );
     }
