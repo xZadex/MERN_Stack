a95504a31d9195f468ba853e7dcf1ce7cd746faa
Nicholas <codebyzade@gmail.com>
2022-10-28 15:16:16 -0400

    More Forms assignment completed

--- Week_01/Day_05/Hook_Form/hook_form/src/components/Form.jsx
+++ Week_01/Day_05/Hook_Form/hook_form/src/components/Form.jsx
@@ -8,25 +8,65 @@ const Form = (props) => {
     const [password, setPassword] = useState("");
     const [password2, setPassword2] = useState("");
     
+
+    const firstValidation = () => {
+        if(first === ""){
+            return ""
+        } else if(first.length < 2){
+            return "First Name must be at least 2 characters"
+        }
+    }
+    const lastValidation = () => {
+        if(last === ""){
+            return ""
+        } else if(last.length < 2){
+            return "Last Name must be at least 2 characters"
+        }
+    }
+    const emailValidation = () => {
+        if(email === ""){
+            return ""
+        } else if(email.length < 2){
+            return "Email must be at least 2 characters"
+        }
+    }
+    const passValidation = () => {
+        if(password === ""){
+            return ""
+        } else if(password.length < 8){
+            return "Password must be at least 8 characters"
+        }
+    }
+    const passConfirmValidation = () => {
+        if(password !== password2){
+            return "Passwords must match"
+        }
+    }
+
     return(
         <div>
         <form>
             <div>
                 <label>First Name: </label> 
-                <input type="text" onChange={ (e) => setFirst(e.target.value) }/>
+                <input type="text" onChange={ (e) => {setFirst(e.target.value)}}/>
             </div>
+            {firstValidation()}
             <div>
                 <label>Last Name: </label> 
                 <input type="text" onChange={ (e) => setLast(e.target.value) }/>
             </div>
+            {lastValidation()}
             <div>
                 <label>Email Address: </label> 
                 <input type="text" onChange={ (e) => setEmail(e.target.value) }/>
             </div>
+            {emailValidation()}
             <div>
                 <label>Password: </label>
                 <input type="text" onChange={ (e) => setPassword(e.target.value) }/>
             </div>
+            {passValidation()}
+            {passConfirmValidation()}
             <div>
                 <label>Confirm Password: </label>
                 <input type="text" onChange={ (e) => setPassword2(e.target.value) }/>
