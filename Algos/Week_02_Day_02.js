7c1025b28acc2ef7b5c3bd5c143524346f66ab92
Nicholas <codebyzade@gmail.com>
2022-11-01 11:47:14 -0400

    morning algo

---
+++ Algos/Week_02_Day_02.js
@@ -0,0 +1,50 @@
+// https://www.hackerrank.com/challenges/diagonal-difference/problem
+/* 
+    Given a square matrix (2d array) of integers Calculate the absolute difference between the sums of its diagonals
+    The absolute difference between two variables, is simply one subtracted from the other and you get the sum.
+    - top left to bottom right diagonal
+    - top right to bottom left diagonal
+*/
+
+const squareMatrix1 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [9, 8, 9],
+];
+const expected1 = 2;
+/* 
+    left to right diagonal: 1 + 5 + 9 = 15
+    right to left diagonal: 3 + 5 + 9 = 17
+    absolute difference = 2
+*/
+
+const squareMatrix2 = [
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+];
+const expected2 = 0;
+/* 
+    left to right diagonal: 1 + 2 + 3 + 4 + 5 = 15
+    right to left diagonal: 5 + 4 + 3 + 2 + 1 = 15
+    absolute difference = 0
+*/
+
+/**
+ * Calculates the absolute diagonal difference of a square matrix.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<Array<number>>} sqrMatrix A 2d array of numbers representing
+ *    a square matrix (rows and columns).
+ * @returns {number} Represents the absolute difference between the top left to
+ *    bottom right diagonal and the top right to bottom left diagonal.
+ */
+const diagonalDifference = (sqrMatrix, sum=0) => {
+    for(let j = 0; j < sqrMatrix.length;j++){ sum+=sqrMatrix[j][j] - sqrMatrix[j][(sqrMatrix[0].length-1)-j]}
+    return Math.abs(sum);
+}
+
+console.log(diagonalDifference(squareMatrix1));
+/*****************************************************************************/
