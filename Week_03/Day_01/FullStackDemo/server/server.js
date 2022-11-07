266c1b81873e8993f3de26628883d82d2a267aad
Nicholas <codebyzade@gmail.com>
2022-11-07 02:14:01 -0500

    full stack demo initial commit

---
+++ Week_03/Day_01/FullStackDemo/server/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+
+app.use( express.json() );
+app.use( express.urlencoded({ extended: true }) );
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello World" });
+});
+
+app.listen( port, () => console.log(`Listening on port: ${port}`) );
